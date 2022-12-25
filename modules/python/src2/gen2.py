539c6206c0d862397c896a95d5ef02af23a97ca0
Vadim Levin <vadim.levin@xperience.ai>
2022-12-25 23:45:01 +0300

    refactor: remove unnecessary textwrap import

--- modules/python/src2/gen2.py
+++ modules/python/src2/gen2.py
@@ -28,27 +28,6 @@ from typing_stubs_generation import (
     ScopeNotFoundError
 )
 
-import textwrap
-
-if hasattr(textwrap, "indent"):
-    indent = textwrap.indent
-else:
-    def indent(text, prefix, predicate=None):
-        """Adds 'prefix' to the beginning of selected lines in 'text'.
-
-        If 'predicate' is provided, 'prefix' will only be added to the lines
-        where 'predicate(line)' is True. If 'predicate' is not provided,
-        it will default to adding 'prefix' to all non-empty lines that do not
-        consist solely of whitespace characters.
-        """
-        if predicate is None:
-            def predicate(line):
-                return line.strip()
-
-        def prefixed_lines():
-            for line in text.splitlines(True):
-                yield prefix + line if predicate(line) else line
-        return ''.join(prefixed_lines())
 
 forbidden_arg_types = ["void*"]
 

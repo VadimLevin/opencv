96e14c7e46272e8e10774e002e8c08f330ad1deb
Vadim Levin <vadim.levin@xperience.ai>
2023-01-17 19:44:07 +0300

    feat: skip typings stubs generation on failure

--- modules/python/src2/typing_stubs_generator.py
+++ modules/python/src2/typing_stubs_generator.py
@@ -1,4 +1,12 @@
+"""Contains a class used to resolve compatibility issues with old Python versions.
+
+Typing stubs generation is available starting from Python 3.6 only.
+For other versions all calls to functions are noop.
+"""
+
 import sys
+import warnings
+
 
 if sys.version_info >= (3, 6):
     from typing import Dict, Set, Any, Sequence
@@ -17,12 +25,57 @@ if sys.version_info >= (3, 6):
         resolve_enum_scopes
     )
 
+    import functools
+
+    class FailuresWrapper:
+        def __init__(self, *, exceptions_as_warnings = True) -> None:
+            self.has_failure = False
+            self.exceptions_as_warnings = exceptions_as_warnings
+
+        def wrap_exceptions_as_warnings(self, original_func=None, *,
+                                        ret_type_on_failure=None):
+            def parametrized_wrapper(func):
+                @functools.wraps(func)
+                def wrapped_func(*args, **kwargs):
+                    if self.has_failure:
+                        if ret_type_on_failure is None:
+                            return None
+                        return ret_type_on_failure()
+
+                    try:
+                        ret_type = func(*args, **kwargs)
+                    except Exception as e:
+                        self.has_failure = True
+                        warnings.warn(
+                            'Typings stubs generation has failed. Reason: {}'.format(e)
+                        )
+                        if ret_type_on_failure is None:
+                            return None
+                        return ret_type_on_failure()
+                    return ret_type
+
+                if self.exceptions_as_warnings:
+                    return wrapped_func
+                else:
+                    return original_func
+
+            if original_func:
+                return parametrized_wrapper(original_func)
+            return parametrized_wrapper
+
+    failures_wrapper = FailuresWrapper(exceptions_as_warnings=True)
+
+    class ClassNodeStub:
+        def add_base(self, base_node):
+            pass
+
     class TypingStubsGenerator:
         def __init__(self):
             self.cv_root = NamespaceNode("cv", export_name="cv2")
             self.exported_enums = {}  # type: Dict[SymbolName, EnumerationNode]
             self.type_hints_ignored_functions = set()  # type: Set[str]
 
+        @failures_wrapper.wrap_exceptions_as_warnings
         def add_enum(self, symbol_name, is_scoped_enum, entries):
             # type: (SymbolName, bool, Dict[str, str]) -> None
             if symbol_name in self.exported_enums:
@@ -37,22 +90,27 @@ if sys.version_info >= (3, 6):
             for entry_name, entry_value in entries.items():
                 enumeration_node.add_constant(entry_name, entry_value)
 
+        @failures_wrapper.wrap_exceptions_as_warnings
         def add_ignored_function_name(self, function_name):
             # type: (str) -> None
             self.type_hints_ignored_functions.add(function_name)
 
+        @failures_wrapper.wrap_exceptions_as_warnings
         def create_function_node(self, func_info):
             # type: (Any) -> None
             create_function_node(self.cv_root, func_info)
 
+        @failures_wrapper.wrap_exceptions_as_warnings(ret_type_on_failure=ClassNodeStub)
         def find_class_node(self, class_info, namespaces):
             # type: (Any, Sequence[str]) -> ClassNode
             return find_class_node(self.cv_root, class_info.full_original_name, namespaces)
 
+        @failures_wrapper.wrap_exceptions_as_warnings(ret_type_on_failure=ClassNodeStub)
         def create_class_node(self, class_info, namespaces):
             # type: (Any, Sequence[str]) -> ClassNode
             return create_class_node(self.cv_root, class_info, namespaces)
 
+        @failures_wrapper.wrap_exceptions_as_warnings
         def generate(self, output_path):
             # type: (str) -> None
             resolve_enum_scopes(self.cv_root, self.exported_enums)
